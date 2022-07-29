c05a9a33a7cc10617612df1a4bdc1bc407f49ae5
21016430-WongChengFeng <21016430@myrp.edu.sg>
2022-07-29 14:42:02 +0800

    Test

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -4,7 +4,7 @@ public class C206_CaseStudy {
 	
 	public static void main(String[] args) {
 		
-//		creating the stallList arraylist aa
+//		creating the stallList arraylist
 		ArrayList<Stall> stallList = new ArrayList<Stall>();
 		
 // 		adding 5 stalls into the stallList		
