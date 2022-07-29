d8efdaf98389156dbb6e96bbfa4b046ad90f69f9
21025571 <21025571@10.174.123.218>
2022-07-29 14:22:54 +0800

    testing

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -4,7 +4,7 @@ public class C206_CaseStudy {
 	
 	public static void main(String[] args) {
 		
-//		creating the stallList arraylist
+//		creating the stallList arraylist jnhbugyvftdcr
 		ArrayList<Stall> stallList = new ArrayList<Stall>();
 		
 // 		adding 5 stalls into the stallList		
