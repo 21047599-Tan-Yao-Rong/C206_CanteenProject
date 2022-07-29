91737fefc28ba69ceb3aaf1283d684b00469e014
21025571 <21025571@10.174.123.218>
2022-07-29 14:19:36 +0800

    I have added the array list for the stalls in the canteen.

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+//		creating the stallList arraylist
+		ArrayList<Stall> stallList = new ArrayList<Stall>();
+		
+// 		adding 5 stalls into the stallList		
+		stallList.add(new Stall("S1", "Drink Store"));
+		stallList.add(new Stall("S2", "Chicken Rice Store"));
+		stallList.add(new Stall("S3", "Western Cuisine"));
+		stallList.add(new Stall("S4", "Japanese Cuisine"));
+		stallList.add(new Stall("S5", "Korean Cuisine"));
+		
+		
 
 	}
 
---
+++ C206_CaseStudy/src/Stall.java
@@ -0,0 +1,21 @@
+
+public class Stall {
+	
+	private String ID;
+	private String Name;
+	
+	public Stall(String iD, String name) 
+	{
+		ID = iD;
+		Name = name;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public String getName() {
+		return Name;
+	}
+	
+}
